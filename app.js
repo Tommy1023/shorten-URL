5281d15a6eab5d31cbf15ec5376cd6d085fd3198
Tommy1023 <j.h.chen1023@gmail.com>
2022-01-26 00:34:53 +0800

    Add express-handlebars,main.hbs and index.hbs

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 const app = express()
 const port = 3000
 
+app.engine('.hbs', exphbs.engine({defaultLayout: 'main', extname: '.hbs'}))
+app.set('view engine', '.hbs')
+
+
 app.get('/', (req, res) => {
-  res.send('This is shorten_url project')
+  res.render('index')
 })
 
 app.listen(port, () => {
